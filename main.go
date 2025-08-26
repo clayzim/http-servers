a77e15ab1ebb142e66f1dad4d02494e46757dd83
clayzim <clayzim118@gmail.com>
2025-08-25 19:05:50 -0500

    Created readiness endpoint

--- main.go
+++ main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
+
+// Readiness endpoint
+// Route: /healthz
+// Method: any
+// Response:
+// 	Headers:
+//		Content-Type: text/plain; charset=utf-8
+//	Body:
+//		"OK"
+func readiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("content-type", "text/plain; charset=utf-8")
+	// For status OK, this could be implicit with w.Write call
+	// TODO: Optionally return 503: Service Unavailable if server isn't ready
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte(http.StatusText(http.StatusOK)))
+	if err != nil {
+		log.Fatalf("failed to write response: %s", err)
+	}
+}
 
 func main() {
 	mux := http.NewServeMux()
 	server := http.Server{Handler: mux, Addr: ":8080"}
 	mux.Handle("/", http.FileServer(http.Dir(".")))
+	// Readiness endpoint path based on Kubernetes pattern
+	mux.HandleFunc("/healthz", readiness)
 
 	server.ListenAndServe()
 }
